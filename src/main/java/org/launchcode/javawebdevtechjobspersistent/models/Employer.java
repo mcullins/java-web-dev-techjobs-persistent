89b9b1dd266304d69f5f9a7f99cefe443508b399
Meg <40369841+mcullins@users.noreply.github.com>
2020-03-04 19:59:33 -0600

    worked to part 2 controllers

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,29 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @ManyToOne
+    @NotBlank
+    private String location;
 
+    public Employer(){}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Max(500)
+    private String description;
+
+    public Skill(){}
+
+    public Skill(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
 }
+
